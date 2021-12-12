e6f0890e806b82fea45ff646a6a6cbae7cda622b
Sujata34 <sujatamrane34@gmail.com>
2021-12-12 11:14:54 +0530

    created new project

---
+++ src/construct/C.java
@@ -0,0 +1,16 @@
+package construct;
+
+public class C extends B{
+	
+	public void mymtd()
+	{
+		System.out.println("in class c");
+	}
+  public static void main(String []jhf)
+  {
+	  C c= new C();
+	  c.show();
+	  c.display();
+	  c.mymtd();
+  }
+}
